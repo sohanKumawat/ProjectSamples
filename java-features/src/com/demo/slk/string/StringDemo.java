f0abc3537c1a64e0c2618d5735b89772d0662971
sohan <sohan@msg.ai>
2017-10-27 11:27:01 +0530

    added string funtionality

---
+++ java-features/src/com/demo/slk/string/StringDemo.java
@@ -0,0 +1,21 @@
+package com.demo.slk.string;
+
+public class StringDemo {
+	
+	
+	public static void k(String s){
+		
+	}
+    public static void k(String s){
+		
+	}
+	
+public static void main(String str[]){
+	
+	String s1=new String("sohan");//hepp
+	String s2=new String("sohan");
+	String s3="sohan";
+	String s4="sohan";
+	System.out.println(s1.equals(s2) +" s1==s2"+(s1==s2) +"s1.e(s3)"+s3.equals(s1)+"s3==s4"+(s3==s4)+"s1==s3"+(s1==s3));
+}
+}
