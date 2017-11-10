41ac9a61a04f1bcbcc092b0887b94f5d27403d59
sohankumawat <sohanmnit@gmail.com>
2017-11-10 23:19:00 +0530

    added myAutoClosable java file

--- java-features/src/com/demo/slk/test/MyAutoClosable.java
+++ java-features/src/com/demo/slk/test/MyAutoClosable.java
@@ -9,14 +9,30 @@ public class MyAutoClosable implements AutoCloseable {
     public void close() throws Exception {
         System.out.println("MyAutoClosable closed!");
     }
-    private static void myAutoClosable() throws Exception {
+    private static void myAutoClosable()  {
         try(MyAutoClosable myAutoClosable = new MyAutoClosable()){
             myAutoClosable.doIt();
         }
+        catch(Exception ex){
+        	
+        }
+    }
+    private static void tryCatch() throws Exception {
+        try{
+        	System.out.println("Inside the tryCatch block");
+        	//throw new Exception();
+        }
+        catch(Exception ex){
+        	ex.printStackTrace();
+        }
+        finally{
+        	System.out.println("Inside the finally block");
+        }
+        
     }
     public static void main(String str[]){
     	try{
-    		MyAutoClosable.myAutoClosable();
+    		MyAutoClosable.tryCatch();
     	}
     	catch(Exception ex){
     		ex.printStackTrace();
