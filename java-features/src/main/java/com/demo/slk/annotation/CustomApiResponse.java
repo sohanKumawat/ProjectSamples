f4d2d31d9ef78fa8e1e2e8f6b04d14875d96328e
sohan <sohan@msg.ai>
2017-12-04 17:51:24 +0530

    added annoation which is used another annotation

---
+++ java-features/src/main/java/com/demo/slk/annotation/CustomApiResponse.java
@@ -0,0 +1,21 @@
+package com.demo.slk.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Successfully retrieved list"),
+        @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+        @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+        @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+})
+public @interface CustomApiResponse {
+
+}
