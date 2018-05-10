e3109dbe7f68d86d4f36c487472c4569bd7508e3
sohank <sohan.kumawat@milkbasket.com>
2018-05-10 16:03:23 +0530

    final working code

--- eureka-server-client-zuul-loadbalancer/service-router-server/src/main/java/com/milkbasket/app/router/service/ServiceRouterManager.java
+++ eureka-server-client-zuul-loadbalancer/service-router-server/src/main/java/com/milkbasket/app/router/service/ServiceRouterManager.java
@@ -2,8 +2,11 @@ package com.milkbasket.app.router.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -11,9 +14,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableZuulProxy
 @EnableDiscoveryClient
 @EnableSwagger2
-//@RestController
+// @RestController
 public class ServiceRouterManager {
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceRouterManager.class, args);
+	}
+
+	@RefreshScope
+	@ConfigurationProperties("zuul")
+	public ZuulProperties zuulProperties() {
+		return new ZuulProperties();
 	}
 }
